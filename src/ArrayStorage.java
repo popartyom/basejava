aa1ce7d625c325018dab0d6b39983158afc05daf
Artyom Popov <pop-artyom@yandex.ru>
2022-08-30 22:50:54 +0300

    ArrayStorage переписаны методы save, get, delete, clear, getAll, size

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -5,50 +5,62 @@ import java.util.*;
  */
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
+    private int size;
+
+    public ArrayStorage() {
+        this.size = size();
+    }
 
     void clear() {
-        Arrays.fill(storage, null);
+        Arrays.fill(storage, 0, size, null);
+        size = 0;
     }
 
     void save(Resume r) {
-        storage[size()] = r;
+        if (size < storage.length) {
+            storage[size] = r;
+            size += 1;
+        } else {
+            System.out.println(r.uuid + "Wasn't saved! The storage is full!");
+        }
     }
 
     Resume get(String uuid) {
-        for (int i = 0; i < size(); i++) {
-            if (uuid.equals(storage[i].uuid)) {
-                return storage[i];
-            }
+        int i = getIndex(uuid);
+        if (i != -1) {
+            return storage[i];
         }
         return null;
     }
 
-    void delete(String uuid) {
-        int found = 0;
-        for (int i = 0; i < size() - 1; i++) {
+    int getIndex(String uuid) {
+        for (int i = 0; i < size; i++) {
             if (uuid.equals(storage[i].uuid)) {
-                found = 1;
-            }
-            if (found == 1) {
-                storage[i] = storage[i + 1];
+                return i;
             }
         }
-        storage[size() - 1] = null;
+        return -1;
+    }
+
+    void delete(String uuid) {
+        int i = getIndex(uuid);
+        storage[i] = storage[size - 1];
+        storage[size - 1] = null;
+        size -= 1;
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return Arrays.copyOfRange(storage, 0, size());
+        return Arrays.copyOf(storage, size);
     }
 
     int size() {
-        for (int i = 0; i < storage.length; i++) {
-            if (storage[i] == null) {
-                return i;
-            }
+        int i = 0;
+        while (storage[i] != null) {
+            i++;
         }
-        return 0;
+        return i;
     }
 }
