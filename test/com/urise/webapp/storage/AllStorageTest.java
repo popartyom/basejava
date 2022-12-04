63795a83a1fb11799f0257e6f348a573243984fd
Artyom Popov <pop-artyom@yandex.ru>
2022-12-04 15:59:24 +0300

    HW6 Test Refactoring new tests

---
+++ test/com/urise/webapp/storage/AllStorageTest.java
@@ -0,0 +1,19 @@
+package com.urise.webapp.storage;
+
+import org.junit.runner.*;
+import org.junit.runners.*;
+
+/**
+ * @author Artyom Popov
+ */
+@RunWith(Suite.class)
+@Suite.SuiteClasses(
+        {
+                ArrayStorageTest.class,
+                SortedArrayStorageTest.class,
+                ListStorageTest.class,
+                MapUuidStorageTest.class
+        })
+public class AllStorageTest {
+
+}
---
+++ test/com/urise/webapp/storage/MapUuidStorageTest.java
@@ -0,0 +1,11 @@
+package com.urise.webapp.storage;
+
+/**
+ * @author Artyom Popov
+ */
+public class MapUuidStorageTest extends AbstractStorageTest{
+
+    public MapUuidStorageTest() {
+        super(new MapUuidStorage());
+    }
+}
