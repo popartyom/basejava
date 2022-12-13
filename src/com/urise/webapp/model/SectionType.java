2aa2edc202a0a5ba5b1a2de27d1730eec39a88a3
Artyom Popov <pop-artyom@yandex.ru>
2022-12-13 22:45:41 +0300

    Enum

---
+++ src/com/urise/webapp/model/SectionType.java
@@ -0,0 +1,24 @@
+package com.urise.webapp.model;
+
+/**
+ * @author Artyom Popov
+ */
+
+public enum SectionType {
+    PERSONAL("Личные качества"),
+    OBJECTIVE("Позиция"),
+    ACHIEVEMENT("Достижения"),
+    QUALIFICATIONS("Квалификация"),
+    EXPERIENCE("Опыт работы"),
+    EDUCATION("Образование");
+
+    private String title;
+
+    SectionType(String title) {
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+}
