63546e059572f21d9e0aaa6b5e42fd7c395e70c3
Artyom Popov <pop-artyom@yandex.ru>
2023-01-20 13:59:25 +0300

    HW8 Start

---
+++ src/com/urise/webapp/storage/AbstractFileStorage.java
@@ -0,0 +1,77 @@
+package com.urise.webapp.storage;
+
+import com.urise.webapp.exception.*;
+import com.urise.webapp.model.*;
+
+import java.io.*;
+import java.util.*;
+
+/**
+ * @author Artyom Popov
+ */
+public abstract class AbstractFileStorage extends AbstractStorage<File> {
+    private final File directory;
+
+    protected AbstractFileStorage(File directory) {
+        Objects.requireNonNull(directory, "directory must not be null");
+        if (!directory.isDirectory()) {
+            throw new IllegalArgumentException(directory.getAbsolutePath() + " is not directory");
+        }
+        if (!directory.canRead() || !directory.canWrite()) {
+            throw new IllegalArgumentException(directory.getAbsolutePath() + " is not readable/writable");
+        }
+        this.directory = directory;
+    }
+
+    protected abstract void doWrite(Resume r, File file) throws IOException;
+
+    @Override
+    protected File getSearchKey(String uuid) {
+        return new File(directory, uuid);
+    }
+
+    @Override
+    protected void doUpdate(Resume r, File file) {
+
+    }
+
+    @Override
+    protected boolean isExist(File file) {
+        return file.exists();
+    }
+
+    @Override
+    protected void doSave(Resume r, File file) {
+        try {
+            file.createNewFile();
+            doWrite(r, file);
+        } catch (IOException e) {
+            throw new StorageException("IO error", file.getName(), e);
+        }
+    }
+
+    @Override
+    protected void doDelete(File file) {
+
+    }
+
+    @Override
+    protected Resume doGet(File file) {
+        return null;
+    }
+
+    @Override
+    protected List<Resume> doCopyAll() {
+        return null;
+    }
+
+    @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+}
