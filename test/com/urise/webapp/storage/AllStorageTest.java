691ce9664c1c965485a5f9734215b6d52a053255
popartyom <pop-artyom@yandex.ru>
2023-06-18 10:18:38 -0500

    DataStreamSerializer

--- test/com/urise/webapp/storage/AllStorageTest.java
+++ test/com/urise/webapp/storage/AllStorageTest.java
@@ -17,7 +17,8 @@ import org.junit.runners.*;
                 ObjectFileStorageTest.class,
                 ObjectPathStorageTest.class,
                 XmlPathStorageTest.class,
-                JsonPathStorageTest.class
+                JsonPathStorageTest.class,
+                DataPathStorageTest.class
         })
 public class AllStorageTest {
 }
