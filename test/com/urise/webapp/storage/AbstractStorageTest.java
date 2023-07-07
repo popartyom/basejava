b5300e1afcf0543b1eeced1b541fd80d47bfbe10
popartyom <pop-artyom@yandex.ru>
2023-07-07 18:44:45 -0500

    Properties

--- test/com/urise/webapp/storage/AbstractStorageTest.java
+++ test/com/urise/webapp/storage/AbstractStorageTest.java
@@ -1,5 +1,6 @@
 package com.urise.webapp.storage;
 
+import com.urise.webapp.Config;
 import com.urise.webapp.exception.*;
 import com.urise.webapp.model.*;
 import org.junit.*;
@@ -14,7 +15,7 @@ import static org.junit.Assert.*;
  * @author Artyom Popov
  */
 public abstract class AbstractStorageTest {
-    protected static final File STORAGE_DIR = new File("C:\\git-javaops\\basejava\\storage");
+    protected static final File STORAGE_DIR = Config.get().getStorageDir();
 
     protected final Storage storage;
 
