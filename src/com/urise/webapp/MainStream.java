efc9cf1f217605130e926a541c6f8e1a153b59df
Artyom Popov <pop-artyom@yandex.ru>
2023-09-13 00:17:01 +0300

    HW10 Streams

--- src/com/urise/webapp/MainStream.java
+++ src/com/urise/webapp/MainStream.java
@@ -1,13 +1,19 @@
 package com.urise.webapp;
 
-import java.util.Arrays;
+import java.util.*;
+import java.util.stream.*;
 
 public class MainStream {
     public static void main(String[] args) {
-
+        int[] values = {1,2,3,3,2,3};
+        System.out.println(minValue(values));
+        List<Integer> integers = Arrays.stream(values)
+                                    .boxed()
+                                    .collect(Collectors.toList());
+        System.out.println(oddOrEven(integers));
     }
 
-    private int minValue(int[] values) {
+    private static int minValue(int[] values) {
         return Arrays.stream(values)
                 .filter(x -> x >= 1)
                 .filter(x -> x <= 9)
@@ -16,8 +22,9 @@ public class MainStream {
                 .reduce(0, (acc, x) -> acc * 10 + x);
     }
 
-//    private List<Integer> oddOrEven(List<Integer> integers) {
-//        return integers.stream()
-//                .collect(Collectors.)
-//    }
+    private static List<Integer> oddOrEven(List<Integer> integers) {
+        final Map<Boolean, List<Integer>> oddsAndEvens = integers.stream()
+                .collect(Collectors.partitioningBy(x -> x % 2 == 0));
+        return oddsAndEvens.get(oddsAndEvens.get(false).size() % 2 != 0);
+    }
 }
